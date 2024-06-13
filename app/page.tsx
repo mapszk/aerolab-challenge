f279c769dc73693806b3bd339b99310c4d66b55b
Martin Pilaszek <martinpilaszek@gmail.com>
2024-06-13 11:56:16 -0300

    Created header section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
+import Container from "@/components/Container";
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main className="bg-red-500 flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="bg-red-500 flex flex-col items-center justify-between">
+      <header className="h-[412px] w-full relative bg-[url(/header-x2.png)] bg-center bg-cover">
+        <Container className="h-full flex">
+          <h1 className="mt-auto mb-10 z-10 text-4xl font-bold text-white">Electronics</h1>
+        </Container>
+      </header>
     </main>
   );
 }
---
+++ components/Container.tsx
@@ -0,0 +1,10 @@
+interface Props {
+  children: React.ReactNode,
+  className?: string
+}
+
+export default function Container({children, className}: Props) {
+  return (
+    <div className={`w-11/12 max-w-[1176px] mx-auto ${className}`}>{children}</div>
+  )
+}
