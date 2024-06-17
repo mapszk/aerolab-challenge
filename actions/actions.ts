3f967ba999e5bdc6e985326fba5dcc848ed197b4
Martin Pilaszek <martinpilaszek@gmail.com>
2024-06-16 21:31:49 -0300

    Removed async await

--- actions/actions.ts
+++ actions/actions.ts
@@ -3,81 +3,86 @@
 import { revalidatePath } from "next/cache";
 
 export async function getProducts() {
-  try {
-    const res = await fetch(process.env.API_BASE_URL + "/products", {
-      headers: {
-        Authorization: `Bearer ${process.env.API_TOKEN}`,
-      },
+  await fetch(process.env.API_BASE_URL + "/products", {
+    headers: {
+      Authorization: `Bearer ${process.env.API_TOKEN}`,
+    },
+  })
+    .then((res) => {
+      if (!res.ok) return res.statusText;
+      else return res.json();
+    })
+    .catch((err) => {
+      console.log(err);
     });
-    if (!res.ok) return res.statusText;
-    else return res.json();
-  } catch (err) {
-    console.log(err);
-  }
 }
 
 export async function getUser() {
-  try {
-    const res = await fetch(process.env.API_BASE_URL + "/user/me", {
-      headers: {
-        Authorization: `Bearer ${process.env.API_TOKEN}`,
-      },
-      next: { tags: ["user"] },
-      cache: "no-store",
+  await fetch(process.env.API_BASE_URL + "/user/me", {
+    headers: {
+      Authorization: `Bearer ${process.env.API_TOKEN}`,
+    },
+    next: { tags: ["user"] },
+    cache: "no-store",
+  })
+    .then((res) => {
+      if (!res.ok) return res.statusText;
+      else return res.json();
+    })
+    .catch((err) => {
+      console.log(err);
     });
-    if (!res.ok) return res.statusText;
-    else return res.json();
-  } catch (err) {
-    console.log(err);
-  }
 }
 
 export async function addPoints(amount: number) {
-  try {
-    revalidatePath("user");
-    const res = await fetch(process.env.API_BASE_URL + "/user/points", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.API_TOKEN}`,
-      },
-      body: JSON.stringify({ amount }),
+  revalidatePath("user");
+  await fetch(process.env.API_BASE_URL + "/user/points", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.API_TOKEN}`,
+    },
+    body: JSON.stringify({ amount }),
+  })
+    .then((res) => {
+      if (!res.ok) return res.statusText;
+      else return res.json();
+    })
+    .catch((err) => {
+      console.log(err);
     });
-    if (!res.ok) return res.statusText;
-    else return res.json();
-  } catch (err) {
-    console.log(err);
-  }
 }
 
 export async function getHistory() {
-  try {
-    const res = await fetch(process.env.API_BASE_URL + "/user/history", {
-      headers: {
-        Authorization: `Bearer ${process.env.API_TOKEN}`,
-      },
+  await fetch(process.env.API_BASE_URL + "/user/history", {
+    headers: {
+      Authorization: `Bearer ${process.env.API_TOKEN}`,
+    },
+  })
+    .then((res) => {
+      if (!res.ok) return res.statusText;
+      else return res.json();
+    })
+    .catch((err) => {
+      console.log(err);
     });
-    if (!res.ok) return res.statusText;
-    else return res.json();
-  } catch (err) {
-    console.log(err);
-  }
 }
 
 export async function redeemProduct(productId: string) {
-  try {
-    revalidatePath("user");
-    const res = await fetch(process.env.API_BASE_URL + "/redeem", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.API_TOKEN}`,
-      },
-      body: JSON.stringify({ productId }),
+  revalidatePath("user");
+  await fetch(process.env.API_BASE_URL + "/redeem", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.API_TOKEN}`,
+    },
+    body: JSON.stringify({ productId }),
+  })
+    .then((res) => {
+      if (!res.ok) return res.statusText;
+      else return res.json();
+    })
+    .catch((err) => {
+      console.log(err);
     });
-    if (!res.ok) return res.statusText;
-    else return res.json();
-  } catch (err) {
-    console.log(err);
-  }
 }
